93d3b91701ddc26e27202bd785e9c4d6d7b52c3b
Dana L. Brown <dana.l.brown@gmail.com>
2020-07-18 14:18:44 -0400

    Create anagram.js

---
+++ anagram.js
@@ -0,0 +1,7 @@
+let isAnagram = function (str1, str2) {
+  let sorted1 = str1.split('').sort().join('').toLowerCase();
+  let sorted2 = str2.split('').sort().join('').toLowerCase();
+ }
+ 
+ console.log(isAnagram('hello', 'jello');
+ console.log9isAnagram ('tar', 'art');
